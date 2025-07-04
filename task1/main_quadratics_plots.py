256103f31c1a9842848de32a7efdf5ab25e1aa4d
Luca <luca24ever@gmail.com>
2025-07-04 16:53:30 +0200

    Small change in plots

--- task1/main_quadratics_plots.py
+++ task1/main_quadratics_plots.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
-import os 
+import os
 
 from Function import QuadraticFunction
 from plot import plot_cost_gradient_norm, plot_cost_quadratic
@@ -28,20 +28,22 @@ GRAPH_TYPES = ["erdos_renyi", "cycle", "star", "path"]
 
 def run_experiment(num_agents, vars_dim, out_dir, seed):
     os.makedirs(os.path.join("figs", out_dir), exist_ok=True)
-    local_loss, global_loss, optimal_z, z0 = setup_quadratic_problem(num_agents=num_agents, vars_dim=vars_dim, seed=seed)
+    local_loss, global_loss, optimal_z, z0 = setup_quadratic_problem(
+        num_agents=num_agents, vars_dim=vars_dim, seed=seed
+    )
     optimal_cost = global_loss(optimal_z)
-    
+
     history_z = {}
     estimated_cost = {}
 
-    #Computing history for all types of graph
+    # Computing history for all types of graph
     for graph_type in GRAPH_TYPES:
         G, A = generate_adj_matrix(
             num_agents,
             connected=True,
             seed=SEED,
             graph_algorithm=graph_type,
-            erdos_renyi_p=0.3 if graph_type == "erdos_renyi" else None
+            erdos_renyi_p=0.3 if graph_type == "erdos_renyi" else None,
         )
 
         history_z[graph_type] = gradient_tracking(
@@ -52,21 +54,25 @@ def run_experiment(num_agents, vars_dim, out_dir, seed):
             alpha=ALPHA,
             epsilon=1e-20,
         )
-       
-        estimated_cost[graph_type] = sum( local_loss[i](history_z[graph_type][-1, i]) for i in range(num_agents) )
-        
-    #Print final cost
+
+        estimated_cost[graph_type] = sum(
+            local_loss[i](history_z[graph_type][-1, i]) for i in range(num_agents)
+        )
+
+    # Print final cost
     print(f"Cost {'optimal':<15}: {optimal_cost:.10f}")
     for graph_type in GRAPH_TYPES:
-        print(f"Cost {graph_type:<15}: {estimated_cost[graph_type]:.10f} | Diff: {abs(estimated_cost[graph_type] - optimal_cost):.10f}")
+        print(
+            f"Cost {graph_type:<15}: {estimated_cost[graph_type]:.10f} | Diff: {abs(estimated_cost[graph_type] - optimal_cost):.10f}"
+        )
 
-    #Plots
-    plt.figure(figsize=(8, 8))
+    # Plots
+    plt.figure(figsize=(16, 16))
     i = 1
     for graph_type in GRAPH_TYPES:
         plt.subplot(2, 2, i)
         plot_cost_quadratic(local_loss, history_z[graph_type], graph_type)
-        plt.plot([optimal_cost]*(NUM_ITERATIONS+1), "--", label="optimum")
+        plt.plot([optimal_cost] * (NUM_ITERATIONS + 1), "--", label="optimum")
         plt.title(graph_type)
         plt.xlabel("$k$")
         plt.ylabel("$l(z^k) (semilog)$")
@@ -75,12 +81,11 @@ def run_experiment(num_agents, vars_dim, out_dir, seed):
         i += 1
 
     plt.tight_layout(pad=1.0)
-    plt.savefig(f"figs/{out_dir}/cost.pdf", bbox_inches="tight")
+    plt.savefig(f"figs/{out_dir}/cost.pdf", bbox_inches="tight", dpi=300)
     plt.close()
-    #plt.show()
+    # plt.show()
 
-    
-    plt.figure(figsize=(8, 8))
+    plt.figure(figsize=(16, 16))
     i = 1
     for graph_type in GRAPH_TYPES:
         plt.subplot(2, 2, i)
@@ -88,14 +93,15 @@ def run_experiment(num_agents, vars_dim, out_dir, seed):
         plt.title(graph_type)
         plt.xlabel("$k$")
         plt.ylabel("$\\left\\Vert \\nabla l(z^k) \\right\\Vert_2$ (log)")
-        plt.yscale('log')
+        plt.yscale("log")
         i += 1
-        #plt.legend(ncol=3, loc="upper center", columnspacing=0.8, labelspacing=0.25, bbox_to_anchor=(0.4, 1.35))
-    
+        # plt.legend(ncol=3, loc="upper center", columnspacing=0.8, labelspacing=0.25, bbox_to_anchor=(0.4, 1.35))
+
     plt.tight_layout(pad=1.0)
-    plt.savefig(f"figs/{out_dir}/gradient.pdf", bbox_inches="tight")
+    plt.savefig(f"figs/{out_dir}/gradient.pdf", bbox_inches="tight", dpi=300)
     plt.close()
-    #plt.show()
+    # plt.show()
+
 
 ################################
 # LOOP PRINCIPALE
@@ -103,4 +109,6 @@ def run_experiment(num_agents, vars_dim, out_dir, seed):
 
 for num_agents in NUM_AGENTS_LIST:
     for vars_dim in NUM_VARS_LIST:
-        run_experiment(num_agents, vars_dim, f"./task_1.1_{num_agents}_{vars_dim}",SEED)
+        run_experiment(
+            num_agents, vars_dim, f"./task_1.1/{num_agents}_{vars_dim}", SEED
+        )
