58f645fb38df961109fce057fbf2702757a2beae
Luca <luca24ever@gmail.com>
2025-07-10 21:34:40 +0200

    Final adjustment

--- task2/main_task_2.1_plots.py
+++ task2/main_task_2.1_plots.py
@@ -3,13 +3,14 @@ from Function import LossFunctionTask2
 from utils import generate_adj_matrix
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-import os 
+import os
 
 plt.rcParams["font.family"] = "cmr10"
 plt.rcParams["mathtext.fontset"] = "cm"
 plt.rcParams["axes.formatter.use_mathtext"] = True
 plt.rcParams["font.size"] = 20
 plt.rcParams["legend.fontsize"] = 13
+
 
 def phi(x):
     return x
@@ -64,18 +65,19 @@ def gradient_tracking_aggregative(
 # PARAMETERS
 #############################
 
-NUM_ROBOTS = [5,15]
+NUM_ROBOTS = [5, 15]
 VAR_DIMS = 2
-SEED = 42
+SEED = 47
 NUM_ITERATIONS = 5000
 ALPHA = lambda k: 2e-3
-#GAMMAS = [[0.1] * NUM_ROBOTS, [0.9]* NUM_ROBOTS]]
+# GAMMAS = [[0.1] * NUM_ROBOTS, [0.9]* NUM_ROBOTS]]
 GAMMAS = [0.1, 0.9]
 GRAPH_TYPES = ["erdos_renyi", "cycle", "star", "path"]
 
+
 def run_experiments(NUM_ROBOTS, VAR_DIMS, GAMMAS, SEED, out_dir):
     #############################
-    # PROBLEM SETUP 
+    # PROBLEM SETUP
     #############################
     rng = np.random.default_rng(SEED)
     os.makedirs(os.path.join("figs", out_dir), exist_ok=True)
@@ -86,7 +88,7 @@ def run_experiments(NUM_ROBOTS, VAR_DIMS, GAMMAS, SEED, out_dir):
     robot_initial_positions = rng.random(size=(NUM_ROBOTS, VAR_DIMS))
     z_history = {}
     s_history = {}
-    v_history= {}
+    v_history = {}
 
     for graph_type in GRAPH_TYPES:
         G, A = generate_adj_matrix(
@@ -97,20 +99,22 @@ def run_experiments(NUM_ROBOTS, VAR_DIMS, GAMMAS, SEED, out_dir):
             erdos_renyi_p=0.3 if graph_type == "erdos_renyi" else None,
         )
 
-        z_history[graph_type],s_history[graph_type], v_history[graph_type]  = gradient_tracking_aggregative(
-            fn_list=loss_functions,
-            z0=robot_initial_positions.copy(),
-            A=A,
-            num_iters=NUM_ITERATIONS,
-            alpha=ALPHA,
-            return_s_v=True,
-            num_agents=NUM_ROBOTS,
+        z_history[graph_type], s_history[graph_type], v_history[graph_type] = (
+            gradient_tracking_aggregative(
+                fn_list=loss_functions,
+                z0=robot_initial_positions.copy(),
+                A=A,
+                num_iters=NUM_ITERATIONS,
+                alpha=ALPHA,
+                return_s_v=True,
+                num_agents=NUM_ROBOTS,
+            )
         )
 
     ##############################
     # PLOT COST AND GRADIENT
     ##############################
-    plt.figure(figsize=(8, 8))
+    plt.figure(figsize=(7, 7))
     for graph_type in GRAPH_TYPES:
         cost_history = [
             sum([loss_functions[i](z_i, np.mean(z)) for i, z_i in enumerate(z)])
@@ -126,16 +130,23 @@ def run_experiments(NUM_ROBOTS, VAR_DIMS, GAMMAS, SEED, out_dir):
     plt.savefig(f"figs/{out_dir}/cost.pdf", bbox_inches="tight", dpi=300)
     plt.close()
 
-    plt.figure(figsize=(8, 8))
+    plt.figure(figsize=(7, 7))
     for graph_type in GRAPH_TYPES:
-        grad_cost_history = np.array([
-            [(
-                loss_functions[i].grad_z(z[i], s[i]) + v[i] * grad_phi(z[i])
-            ) 
-            for i in range(NUM_ROBOTS)]
-            for z,v,s in zip(z_history[graph_type], v_history[graph_type], s_history[graph_type])
-        ])
-        grad_cost_history_sum = np.linalg.norm(np.sum(grad_cost_history, axis=1), axis=1)
+        grad_cost_history = np.array(
+            [
+                [
+                    (loss_functions[i].grad_z(z[i], s[i]) + v[i] * grad_phi(z[i]))
+                    for i in range(NUM_ROBOTS)
+                ]
+                for z, v, s in zip(
+                    z_history[graph_type], v_history[graph_type], s_history[graph_type]
+                )
+            ]
+        )
+        print(grad_cost_history.shape)
+        grad_cost_history_sum = np.linalg.norm(
+            np.sum(grad_cost_history, axis=1), axis=1
+        )
         plt.plot(grad_cost_history_sum, label=graph_type)
     plt.xlabel("$k$")
     plt.ylabel(r"$\|\nabla \ell_i(z_i, \sigma(z))\|$")
@@ -146,7 +157,14 @@ def run_experiments(NUM_ROBOTS, VAR_DIMS, GAMMAS, SEED, out_dir):
     plt.savefig(f"figs/{out_dir}/gradient.pdf", bbox_inches="tight", dpi=300)
     plt.close()
 
+
 for num_robots in NUM_ROBOTS:
     for gamma in GAMMAS:
-        gammas = [gamma]*num_robots
-        run_experiments(NUM_ROBOTS=num_robots, VAR_DIMS=VAR_DIMS, GAMMAS=gammas, SEED=SEED, out_dir=f"./task_2.1/{num_robots}_{gamma}")
+        gammas = [gamma] * num_robots
+        run_experiments(
+            NUM_ROBOTS=num_robots,
+            VAR_DIMS=VAR_DIMS,
+            GAMMAS=gammas,
+            SEED=SEED,
+            out_dir=f"./task_2.1/{num_robots}_{gamma}",
+        )
--- task2/main_task_2.2_plots.py
+++ task2/main_task_2.2_plots.py
@@ -2,12 +2,15 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from utils import generate_adj_matrix
+from Function import LossFunctionTask2
 
 plt.rcParams["font.family"] = "cmr10"
 plt.rcParams["mathtext.fontset"] = "cm"
 plt.rcParams["axes.formatter.use_mathtext"] = True
 plt.rcParams["font.size"] = 20
 plt.rcParams["legend.fontsize"] = 13
+plt.rcParams["text.usetex"] = True
 
 
 GRAPH_METHODS = {
@@ -17,80 +20,175 @@ GRAPH_METHODS = {
     "star": "Star",
 }
 
-def merge_dfs(folder_path):
 
+def merge_dfs(folder_path):
     # List all files in the directory
-    files = [f for f in os.listdir(folder_path) if f.endswith('.csv') and f.startswith('robot_')]
+    files = [
+        f
+        for f in os.listdir(folder_path)
+        if f.endswith(".csv") and f.startswith("robot_")
+    ]
     dfs = []
     for file in files:
         # Read the CSV file into a DataFrame
         df = pd.read_csv(os.path.join(folder_path, file))
         # Add a new column for the graph method based on the filename
-        if len(file.split('_')) < 3:
+        if len(file.split("_")) < 3:
             graph_method = ""
         else:
-            graph_method = file.split('_')[2].split('.')[0]  # Extract the graph
-        df['graph_method'] = GRAPH_METHODS.get(graph_method, 'Erdős-Rényi')
+            graph_method = file.split("_")[2].split(".")[0]  # Extract the graph
+        df["graph_method"] = GRAPH_METHODS.get(graph_method, "Erdős-Rényi")
 
         dfs.append(df)
-    
+
     return pd.concat(dfs, ignore_index=True)
 
 
-
-def plot(df, col,num_robots, sum_over_robots=False, plot_single_robots=True, label=""):
+def plot(df, col, num_robots, label=""):
     robots_values = []
     for i in range(num_robots):
-        robot_df = df[df['robot_id'] == i]
-        values = [np.linalg.norm(val) if isinstance(val, np.ndarray) else val for val in robot_df[col].to_numpy()]        
-        if sum_over_robots:
-            robots_values.append(values)
-        if plot_single_robots:
-            plt.plot(robot_df['iteration'].to_numpy(), values, label=f'Robot {i}', linewidth=1.5, alpha=0.7, linestyle='--')
-    
-    if sum_over_robots:
-        summed_values = np.sum(np.array(robots_values), axis=0)
-        plt.plot(robot_df['iteration'].to_numpy(), summed_values, label=label)
-        
-    plt.xlabel('Iteration')
-    plt.yscale('log')
+        robot_df = df[df["robot_id"] == i]
+        values = np.array([val for val in robot_df[col]])
+        robots_values.append(values)
+
+    summed_values = np.sum(np.array(robots_values), axis=0)
+    if len(summed_values.shape) == 2:
+        summed_values = np.linalg.norm(summed_values, axis=1)
+    plt.plot(robot_df["iteration"].to_numpy(), summed_values, label=label)
+
+    plt.xlabel("Iteration")
+    plt.yscale("log")
     plt.grid()
 
 
 def experiment_plot(num_robots, gamma, base_dir):
+    SEED = 47
+    VAR_DIMS = 2
+    NUM_ITERATIONS = 5000
+    ALPHA = 2e-3
+    SIMULATION_HZ = 100
+
+    rng = np.random.default_rng(SEED)
+    PRIVATE_TARGETS = rng.random(size=(num_robots, VAR_DIMS))
+    ROBOT_INITIAL_POSITIONS = rng.random(size=(num_robots, VAR_DIMS))
     df = merge_dfs(base_dir)
 
-    # Parse position columns, not it is in format "[a,b,c]"
-    for col in ["position", "target", "sigma_est", "grad_est", "grad_z", "grad_sigma_z"]:
-        df[col] = df[col].apply(lambda x: np.fromstring(x.strip('[]'), sep=','))
+    # extract z_history in the format (5001, 5, 2)
+    z_history = {}
+    for g, graph in GRAPH_METHODS.items():
+        if g == "":
+            g = "erdos_renyi"
+        G, A = generate_adj_matrix(
+            num_robots,
+            connected=True,
+            seed=SEED,
+            graph_algorithm=g,
+            erdos_renyi_p=0.3,
+        )
+        loss_functions = [
+            LossFunctionTask2(PRIVATE_TARGETS[i], gamma) for i in range(num_robots)
+        ]
+        zz = []
+        for robot in range(num_robots):
+            robot_df = df[(df["robot_id"] == robot) & (df["graph_method"] == graph)]
 
+            zz.append(
+                np.array(
+                    [
+                        np.fromstring(x.strip("[]"), sep=",")
+                        for x in robot_df["position"]
+                    ]
+                )
+            )
+        z_history[graph] = np.transpose(np.array(zz), (1, 0, 2))
+        print(f"z_history[{graph}].shape: {z_history[graph].shape}")
+
+    plt.figure(figsize=(7, 7))
+    for graph_type in GRAPH_METHODS.values():
+        cost_history = [
+            sum([loss_functions[i](z_i, np.mean(z)) for i, z_i in enumerate(z)])
+            for z in z_history[graph_type]
+        ]
+        plt.plot(cost_history, label=graph_type)
+    plt.xlabel("$k$")
+    plt.ylabel("$l(z^k) (log)$")
+    plt.yscale("log")
+    plt.grid()
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(
+        f"figs/task_2.2/{num_robots}_{gamma:.1f}/cost.pdf", bbox_inches="tight", dpi=300
+    )
+    plt.close()
+    # Parse position columns, not it is in format "[a,b,c]"
+    for col in [
+        "position",
+        "target",
+        "sigma_est",
+        "grad_est",
+        "grad_z",
+        "grad_sigma_z",
+    ]:
+        df[col] = df[col].apply(lambda x: np.fromstring(x.strip("[]"), sep=","))
 
     if not os.path.exists("figs/task_2.2"):
         os.makedirs("figs/task_2.2")
 
-    plt.figure(figsize=(12, 6), dpi=300)
-    plt.subplot(1, 2, 1)
-    for graph in GRAPH_METHODS.values():
-        graph_df = df[df['graph_method'] == graph]
-        if not graph_df.empty:
-            plot(graph_df, 'local_cost', num_robots, sum_over_robots=True, plot_single_robots=False, label=graph)
-    # plot(df, 'local_cost',num_robots, sum_over_robots=True, plot_single_robots=False)
-    plt.ylabel(r'$\ell_i(z_i, \sigma(z))$')
-    plt.title('Local Cost Function')
-    plt.legend(loc='upper right')
+    # plt.figure(figsize=(7, 7), dpi=300)
+    # # plt.subplot(1, 2, 1)
+    # for graph in GRAPH_METHODS.values():
+    #     graph_df = df[df["graph_method"] == graph]
+    #     if not graph_df.empty:
+    #         plot(
+    #             graph_df,
+    #             "local_cost",
+    #             num_robots,
+    #             label=graph,
+    #         )
+    # # plot(df, 'local_cost',num_robots, sum_over_robots=True, plot_single_robots=False)
+    # # plt.ylabel(r"$\ell_i(z_i, \sigma(z))$")
+    # plt.ylabel("$l(z^k) (log)$")
+    # plt.yscale("log")
+    # # plt.title("Local Cost Function")
+    # plt.grid()
+    # plt.xlabel("$k$")
+    # plt.legend()
+    # plt.tight_layout()
+    # if not os.path.exists(
+    #     os.path.join("figs", "task_2.2", f"{num_robots}_{gamma:.1f}")
+    # ):
+    #     os.makedirs(os.path.join("figs", "task_2.2", f"{num_robots}_{gamma:.1f}"))
 
-    plt.subplot(1, 2, 2)
+    # plt.savefig(
+    #     os.path.join("figs", "task_2.2", f"{num_robots}_{gamma:.1f}", "cost.pdf"),
+    #     bbox_inches="tight",
+    #     dpi=300,
+    # )
+    # plt.close()
+
+    plt.figure(figsize=(7, 7), dpi=300)
     for graph in GRAPH_METHODS.values():
-        graph_df = df[df['graph_method'] == graph]
+        graph_df = df[df["graph_method"] == graph]
         if not graph_df.empty:
-            plot(graph_df, 'grad_est', num_robots, sum_over_robots=True, plot_single_robots=False, label=graph)
-    plt.title('Norm of the gradient')
-    plt.xlabel('Iteration')
+            plot(
+                graph_df,
+                "grad_est",
+                num_robots,
+                label=graph,
+            )
+    # plt.title("Norm of the gradient")
+    plt.xlabel("k")
+
     plt.ylabel(r"$\|\nabla \ell_i(z_i, \sigma(z))\|$")
-    plt.legend(loc='upper right')
-
+    plt.legend()
+    plt.yscale("log")
+    plt.grid()
     plt.tight_layout()
-    plt.savefig(os.path.join("figs", "task_2.2", f"{num_robots}_{gamma:.1f}.pdf"), dpi=300)
+
+    plt.savefig(
+        os.path.join("figs", "task_2.2", f"{num_robots}_{gamma:.1f}", "gradient.pdf"),
+        dpi=300,
+    )
     plt.close()
 
 
